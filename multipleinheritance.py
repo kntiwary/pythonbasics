31759e7d8c99e02fb54ec4a3578b16ff2220b4c1
Kamta Nath Tiwary <Kamta@skillworks.in>
2019-03-13 13:00:45 +0530

    operatoroverloading

--- multipleinheritance.py
+++ multipleinheritance.py
@@ -5,4 +5,27 @@
 # This order is also called linearization of MultiDerived class and the set of
 # rules used to find this order is called Method Resolution Order (MRO).
 
+class Base1:
+    pass
 
+class Base2:
+    pass
+
+class Derived(Base1,Base2):
+    pass
+
+# print (Derived.__mro__)
+
+
+class X: pass
+class Y: pass
+class Z: pass
+
+class A(X,Y): pass
+class B(Y,Z): pass
+
+class M(B,A,Z): pass
+class N(A,B,Z): pass
+
+print(M.mro())
+print(N.mro())
